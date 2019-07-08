487a2f0f0f1c68a19503efd381574a0b45d8f90d
imanshafiei <imanshafiei@ymail.com>
2019-07-08 14:40:47 +0430

    printing improve

--- printerServer/printer/views.py
+++ printerServer/printer/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 import time
 import random
 import json
-#import win32api, win32print, time
+# import win32api, win32print, time
 import pandas as pd
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Image, Spacer
 from reportlab.lib import colors, pagesizes, units
@@ -45,101 +45,162 @@ def get_farsi_bulleted_text(text, wrap_length=None):
         return '<font>%s</font>' % farsi_text
     return '<font>%s &#x02022;</font>' % farsi_text
 
+
 GHOSTSCRIPT_PATH = "C:/Program Files/gs/gs9.27/bin/gswin64.exe"
 GSPRINT_PATH = 'C:/Program Files/gs/gsprint/gsprint.exe'
 FILE = 'C:/Users/CafeBoard/Desktop/file.html'
 
+
 @csrf_exempt
 def print_something(request):
     currentprinter = 'Cash'
-    #win32print.SetDefaultPrinter('NowKitchen')
-    #win32api.ShellExecute(0, "print", 'C:/Users/CafeBoard/Desktop/file.pdf', None, ".", 0)
-    #params = '-ghostscript "' + GHOSTSCRIPT_PATH + '" -printer "' + currentprinter + '" -copies 1 "C:/Users/CafeBoard/Desktop/file.html "'
-    #print(params)
+    # win32print.SetDefaultPrinter('NowKitchen')
+    # win32api.ShellExecute(0, "print", 'C:/Users/CafeBoard/Desktop/file.pdf', None, ".", 0)
+    # params = '-ghostscript "' + GHOSTSCRIPT_PATH + '" -printer "' + currentprinter + '" -copies 1 "C:/Users/CafeBoard/Desktop/file.html "'
+    # print(params)
     import os
 
-    #os.startfile("C:/Users/CafeBoard/Desktop/file.pdf", "print")
-    #win32api.ShellExecute(0, 'open', GSPRINT_PATH, params, 'K', 0)
-    #time.sleep(1)
-    #win32print.SetDefaultPrinter('NowBar')
-    #win32api.ShellExecute(0, "print", 'C:/Users/CafeBoard/Desktop/file.html', None, ".", 0)
-    #time.sleep(1)
-    #win32print.SetDefaultPrinter('Cash')
+    # os.startfile("C:/Users/CafeBoard/Desktop/file.pdf", "print")
+    # win32api.ShellExecute(0, 'open', GSPRINT_PATH, params, 'K', 0)
+    # time.sleep(1)
+    # win32print.SetDefaultPrinter('NowBar')
+    # win32api.ShellExecute(0, "print", 'C:/Users/CafeBoard/Desktop/file.html', None, ".", 0)
+    # time.sleep(1)
+    # win32print.SetDefaultPrinter('Cash')
 
     data = request.POST
     print(data)
     for e in data:
         data = json.loads(e)
-    doc = SimpleDocTemplate("form_letter.pdf", pagesize=(80 * units.mm, 200 * units.mm),
-                            rightMargin=72, leftMargin=72,
-                            topMargin=12, bottomMargin=18)
-    pdfmetrics.registerFont(TTFont('IRANSANS', '/Users/impala69/Desktop/font.ttf'))
-    Story = []
-    logo = "/Users/impala69/Desktop/boardlogored.png"
-    limitedDate = "03/05/2010"
-
-    formatted_time = time.ctime()
-    full_name = "Infosys"
-
-    im = Image(logo, 1 * units.inch, 1 * units.inch, hAlign='LEFT')
-    Story.append(im)
-    Story.append(Spacer(1, 12))
-
-    styles = getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY, fontName='IRANSANS'))
-    styles.add(ParagraphStyle(name='RIGHT', alignment=TA_RIGHT, fontName='IRANSANS'))
-    styles.add(ParagraphStyle(name='Persian', alignment=TA_CENTER, fontName='IRANSANS'))
-
-    ptext = '<font size=6>%s</font>' % formatted_time
-    Story.append(Paragraph(ptext, styles["Persian"]))
-    Story.append(Spacer(1, 12))
-
-    if data['is_customer_print'] == 1:
-        ptext = '<font size=8>%s</font>' % get_farsi_bulleted_text(data['customer_name'], wrap_length=120)
-        Story.append(Paragraph(ptext, styles["Persian"]))
-        Story.append(Spacer(1, 12))
-
-    ptext = '<font size=10>%s</font>' % data['table_name']
-    Story.append(Paragraph(get_farsi_bulleted_text(ptext, wrap_length=120), styles["Persian"]))
-    Story.append(Spacer(1, 12))
 
     if data['is_customer_print'] == 0:
         invoice_data = []
         item_data_list = []
-        for item in data['items']:
-            item_data_list.append(Paragraph(get_farsi_bulleted_text(str(item['numbers']), wrap_length=120), styles["Persian"]))
-            item_data_list.append(Paragraph(get_farsi_bulleted_text(item['name'], wrap_length=120), styles["Persian"]))
-            invoice_data.append(item_data_list)
-            item_data_list = []
+        for printer_data in data['data']:
+            printer_name = printer_data['printer_name']
+            if len(printer_data['items']) == 0:
+                break
 
-        t_style = TableStyle([
-            ('ALIGN', (1, 1), (-2, -2), 'RIGHT'),
-            ('TEXTCOLOR', (1, 1), (-2, -2), colors.black),
-            ('VALIGN', (0, 0), (0, -1), 'RIGHT'),
-            ('TEXTCOLOR', (0, 0), (0, -1), colors.black),
-            ('ALIGN', (0, -1), (-1, -1), 'CENTER'),
-            ('TEXTFONT', (0, -1), (-1, -1), 'IRANSANS'),
-            ('VALIGN', (0, -1), (-1, -1), 'RIGHT'),
-            ('TEXTFONT', (0, -1), (-1, -1), 'IRANSANS'),
-            ('TEXTCOLOR', (0, -1), (-1, -1), colors.black),
-            ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-            ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
-        ])
+            # config printers
 
-        t = Table(invoice_data, colWidths=20 * units.mm)
-        t.setStyle(t_style)
+            doc = SimpleDocTemplate("form_letter.pdf", pagesize=(80 * units.mm, 200 * units.mm),
+                                    rightMargin=72, leftMargin=72,
+                                    topMargin=12, bottomMargin=18)
+            pdfmetrics.registerFont(TTFont('IRANSANS', '/Users/impala69/Desktop/font.ttf'))
+            Story = []
+            logo = "/Users/impala69/Desktop/boardlogored.png"
+            limitedDate = "03/05/2010"
 
-        Story.append(t)
-        Story.append(Spacer(1, 12))
+            formatted_time = time.ctime()
+            full_name = "Infosys"
 
-        doc.build(Story)
+            im = Image(logo, 1 * units.inch, 1 * units.inch, hAlign='LEFT')
+            Story.append(im)
+            Story.append(Spacer(1, 12))
+
+            styles = getSampleStyleSheet()
+            styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY, fontName='IRANSANS'))
+            styles.add(ParagraphStyle(name='RIGHT', alignment=TA_RIGHT, fontName='IRANSANS'))
+            styles.add(ParagraphStyle(name='Persian', alignment=TA_CENTER, fontName='IRANSANS'))
+
+            ptext = '<font size=6>%s</font>' % formatted_time
+            Story.append(Paragraph(ptext, styles["Persian"]))
+            Story.append(Spacer(1, 12))
+
+            ptext = '<font size=10>%s</font>' % data['table_name']
+            Story.append(Paragraph(get_farsi_bulleted_text(ptext, wrap_length=120), styles["Persian"]))
+            Story.append(Spacer(1, 12))
+
+            # end config
+
+            for item in printer_data['items']:
+                item_data_list.append(
+                    Paragraph(get_farsi_bulleted_text(str(item['numbers']), wrap_length=120), styles["Persian"]))
+                item_data_list.append(
+                    Paragraph(get_farsi_bulleted_text(item['name'], wrap_length=120), styles["Persian"]))
+                invoice_data.append(item_data_list)
+                item_data_list = []
+
+            t_style = TableStyle([
+                ('ALIGN', (1, 1), (-2, -2), 'RIGHT'),
+                ('TEXTCOLOR', (1, 1), (-2, -2), colors.black),
+                ('VALIGN', (0, 0), (0, -1), 'RIGHT'),
+                ('TEXTCOLOR', (0, 0), (0, -1), colors.black),
+                ('ALIGN', (0, -1), (-1, -1), 'CENTER'),
+                ('TEXTFONT', (0, -1), (-1, -1), 'IRANSANS'),
+                ('VALIGN', (0, -1), (-1, -1), 'RIGHT'),
+                ('TEXTFONT', (0, -1), (-1, -1), 'IRANSANS'),
+                ('TEXTCOLOR', (0, -1), (-1, -1), colors.black),
+                ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+            ])
+
+            t = Table(invoice_data, colWidths=20 * units.mm)
+            t.setStyle(t_style)
+
+            Story.append(t)
+            Story.append(Spacer(1, 12))
+
+            doc.build(Story)
+            invoice_data = []
+            print("printing in: %s" % printer_name)
 
     else:
+
+        doc = SimpleDocTemplate("form_letter.pdf", pagesize=(80 * units.mm, 200 * units.mm),
+                                rightMargin=72, leftMargin=72,
+                                topMargin=12, bottomMargin=18)
+        pdfmetrics.registerFont(TTFont('IRANSANS', '/Users/impala69/Desktop/font.ttf'))
+        Story = []
+        logo = "/Users/impala69/Desktop/boardlogored.png"
+        limitedDate = "03/05/2010"
+
+        formatted_time = time.ctime()
+        full_name = "Infosys"
+
+        im = Image(logo, 1 * units.inch, 1 * units.inch, hAlign='LEFT')
+        Story.append(im)
+        Story.append(Spacer(1, 12))
+
+        styles = getSampleStyleSheet()
+        styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY, fontName='IRANSANS'))
+        styles.add(ParagraphStyle(name='RIGHT', alignment=TA_RIGHT, fontName='IRANSANS'))
+        styles.add(ParagraphStyle(name='Persian', alignment=TA_CENTER, fontName='IRANSANS'))
+
+        ptext = '<font size=6>%s</font>' % formatted_time
+        Story.append(Paragraph(ptext, styles["Persian"]))
+        Story.append(Spacer(1, 12))
+
+        if data['is_customer_print'] == 1:
+            ptext = '<font size=8>%s</font>' % get_farsi_bulleted_text(data['customer_name'], wrap_length=120)
+            Story.append(Paragraph(ptext, styles["Persian"]))
+            Story.append(Spacer(1, 12))
+
+        ptext = '<font size=10>%s</font>' % data['table_name']
+        Story.append(Paragraph(get_farsi_bulleted_text(ptext, wrap_length=120), styles["Persian"]))
+        Story.append(Spacer(1, 12))
+
         invoice_data = []
         item_data_list = []
+
+        item_data_list.append(
+            Paragraph(get_farsi_bulleted_text('کل', wrap_length=120), styles["Persian"]))
+        item_data_list.append(
+            Paragraph(get_farsi_bulleted_text("تعداد", wrap_length=120), styles["Persian"]))
+        item_data_list.append(
+            Paragraph(get_farsi_bulleted_text("فی", wrap_length=120), styles["Persian"]))
+        item_data_list.append(Paragraph(get_farsi_bulleted_text("نام", wrap_length=120), styles["Persian"]))
+        invoice_data.append(item_data_list)
+
+        item_data_list = []
+
         for item in data['items']:
-            item_data_list.append(Paragraph(get_farsi_bulleted_text(str(item['price']), wrap_length=120), styles["Persian"]))
-            item_data_list.append(Paragraph(get_farsi_bulleted_text(str(item['numbers']), wrap_length=120), styles["Persian"]))
+            item_data_list.append(
+                Paragraph(get_farsi_bulleted_text(str(item['price']), wrap_length=120), styles["Persian"]))
+            item_data_list.append(
+                Paragraph(get_farsi_bulleted_text(str(item['numbers']), wrap_length=120), styles["Persian"]))
+            item_data_list.append(
+                Paragraph(get_farsi_bulleted_text(str(item['item_price']), wrap_length=120), styles["Persian"]))
             item_data_list.append(Paragraph(get_farsi_bulleted_text(item['name'], wrap_length=120), styles["Persian"]))
             invoice_data.append(item_data_list)
             item_data_list = []
@@ -158,15 +219,12 @@ def print_something(request):
             ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
         ])
 
-        t = Table(invoice_data, colWidths=20 * units.mm)
+        t = Table(invoice_data, colWidths=18 * units.mm)
         t.setStyle(t_style)
 
         Story.append(t)
         Story.append(Spacer(1, 12))
 
         doc.build(Story)
-
-    for printer in data['printers']:
-        print("Printing in %s" % printer)
 
     return JsonResponse({"response": 'OK'})
